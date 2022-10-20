3740137304f0f260f8e91792219f1430f46dd461
moulali <50131086+Nadendla5@users.noreply.github.com>
2022-10-20 14:01:10 -0400

    Add files via upload

---
+++ Webapi/Controllers/CompanyController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Routing;
+using AutoMapper;
+using BusinessLayer.Model.Interfaces;
+using DataAccessLayer.Model.Models;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class CompanyController : ApiController
+    {
+        private readonly ICompanyService _companyService;
+        private readonly IMapper _mapper;
+        private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        public CompanyController(ICompanyService companyService, IMapper mapper)
+        {
+            _companyService = companyService;
+            _mapper = mapper;
+        }
+        // GET api/<controller>]
+        [HttpGet]
+        [Route("api/company/get")]
+        public async Task<IEnumerable<CompanyDto>> GetAll()
+        {
+            _logger.Info($"Getting All Companies");
+            try
+            {
+                var items = await _companyService.GetAllCompanies();
+                return _mapper.Map<IEnumerable<CompanyDto>>(items);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error while getting All Companies,Exception => {ex}");
+                throw new Exception("Error While Getting companies details");
+            }           
+        }
+
+        // GET api/<controller>/5
+        [HttpGet]
+        [Route("api/company/get/{companyCode}")]
+        public async Task<CompanyDto> Get(string companyCode)
+        {
+            _logger.Info($"Getting details for CompanyCode {companyCode}");
+            try
+            {
+                var item = await _companyService.GetCompanyByCode(companyCode);
+                return _mapper.Map<CompanyDto>(item);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error while getting All CompanyDetails,Exception => {ex.Message}");
+                throw new Exception("Error While Getting company details");
+            }
+      
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        [Route("api/company/save")]
+        public async Task<string> Post([FromBody] Company company)
+        {
+            _logger.Info($"Saving Company Details");
+            try
+            {
+                var result = await _companyService.SaveCompany(company);
+                if (result)
+                    return "Saved Succesfully";
+                _logger.Error($"Error While saving Company Details");
+                throw new Exception("Error While saving Company Details");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error While saving Company Details,Exception => {ex.Message}");
+                throw new Exception("Error While saving Company Details");
+            }
+            
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        [Route("api/company/update/{id}")]
+        public async Task<string> Put(int id, [FromBody] Company company)
+        {
+            try
+            {
+                var result = await _companyService.SaveCompany(company);        
+                if (result)
+                    return "Updated Succesfully";
+                _logger.Error($"Error While Updating Company Details");
+                throw new Exception("Error While Updating Company Details");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error While Updating Company Details,Exception => {ex.Message}");
+                throw new Exception("Error While Updating Company Details");
+            }
+            
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete]
+        [Route("api/company/delete/{id}")]
+        public async Task<string> Delete(int id)
+        {
+            try
+            {
+                var result = await _companyService.DeleteCompany(id);
+                
+                if (result)
+                    return "Removed Succesfully";
+                _logger.Error($"Error While Removing Company Details");
+                throw new Exception("Error While Removing Company Details");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error While Removing Company Details,Exception => {ex.Message}");
+                throw new Exception("Error While Removing Company Details");
+            }
+           
+        }
+    }
+}
---
+++ Webapi/Controllers/EmployeeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using BusinessLayer.Model.Interfaces;
+using DataAccessLayer.Model.Models;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IMapper _mapper;
+        private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public EmployeeController(IEmployeeService employeeService, IMapper mapper)
+        {
+            _employeeService = employeeService;
+            _mapper = mapper;
+        }
+        // GET api/<controller>
+        [HttpGet]
+        [Route("api/employee/get")]
+        public async Task<IEnumerable<EmployeeDto>> GetAll()
+        {
+            _logger.Info($"Getting All Employees");
+            try
+            {
+                var items = await _employeeService.GetAllEmployees();
+                return _mapper.Map<IEnumerable<EmployeeDto>>(items);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error while getting All Employees,Exception => {ex}");
+                throw new Exception("Error While Getting Employees");
+            }
+            
+        }
+
+        // GET api/<controller>/5
+        [HttpGet]
+        [Route("api/employee/get/{employeeCode}")]
+        public async Task<EmployeeDto> Get(string employeeCode)
+        {
+            _logger.Info($"Getting details for EmployeeCode {employeeCode}");
+            try
+            {
+                var item = await _employeeService.GetEmployeeByCode(employeeCode);
+                return _mapper.Map<EmployeeDto>(item);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error while getting All CompanyDetails,Exception => {ex.Message}");
+                throw new Exception("Error While Getting company details");
+            }
+           
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        [Route("api/employee/save")]
+        public async Task<string> Post([FromBody]Employee employee)
+        {
+            _logger.Info($"Saving Company Details");
+            try
+            {
+                var result = await _employeeService.SaveEmployee(employee);
+                if (result)
+                    return "Saved Succesfully";
+                _logger.Error($"Error While saving Employee Details");
+                throw new Exception("Error While saving Employee Details");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error While saving Employee Details,Exception => {ex.Message}");
+                throw new Exception("Error While saving Employee Details");
+            }
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        [Route("api/employee/update/{id}")]
+        public async Task<string> Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                var result = await _employeeService.SaveEmployee(employee);
+                if (result)
+                    return "Updated Succesfully";
+                _logger.Error($"Error While Updating Employee Details");
+                throw new Exception("Error While Updating Employee Details");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error While Updating Employee Details,Exception => {ex.Message}");
+                throw new Exception("Error While Updating Employee Details");
+            }
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete]
+        [Route("api/employee/delete/{id}")]
+        public async Task<string> Delete(int id)
+        {
+            try
+            {
+                var result = await _employeeService.DeleteEmployee(id);
+
+                if (result)
+                    return "Removed Succesfully";
+                _logger.Error($"Error While Removing Employee Details");
+                throw new Exception("Error While Removing Employee Details");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error While Removing Employee Details,Exception => {ex.Message}");
+                throw new Exception("Error While Removing Employee Details");
+            }
+        }
+    }
+}
